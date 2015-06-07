13e6a1bbf8b1d065661736a074b08f3a5dfcac88
Thepanjan <purple.prize@gmail.com>
2015-06-08 00:30:54 +0530

    Course Project 1 Submission

---
+++ Project1_codes_and_graphs/plot1.R
@@ -0,0 +1,6 @@
+install.packages("sqldf")
+library(sqldf)	
+data<-read.csv.sql("household_power_consumption.txt",sql="select * from file where Date in ('1/2/2007','2/2/2007')",header=TRUE,stringsAsFactors=FALSE,sep=';')
+png("./plot1.png",width=480,height=480)
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="orangered2")
+dev.off()
